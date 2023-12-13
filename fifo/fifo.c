64e5ece0fac7994aa4f65c7b0c43fcefc3e383aa
AbhiRagh <abhiragh0@gmail.com>
2023-12-13 21:55:15 +0530

    Updated fifo code

--- fifo/fifo.c
+++ fifo/fifo.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
-void main(){
-    int i,j,n,a[50],frame[10],no,k,avail,count=0;
-    printf("\nEnter The Number Of Pages: \n");
-    scanf("%d",&n);
-    printf("\n Enter The Page Number:\n");
-    for(i=1;i<=n;i++)
-        scanf("%d",&a[i]);
-    printf("\nEnter The Number of Frames:\n");
-    scanf("%d",&no);
-    for(i=0;i<no;i++)
-        frame[i]=1;
-    j=0;
-    printf("\tRef String\t Page Frames\n");
-    for(i=1;i<=n;i++){
-        printf("%d\t\t",a[i]);
-        avail=0;
-        for(k=0;k<no;k++)
-            if(frame[k]==a[i])
-                avail=1;
-            if(avail==0){
-                frame[j]=a[i];
-                j=(j+1)%no;
-                count++;
-                for(k=0;k<no;k++)
-                    printf("%d\t",frame[k]);
+
+int main() {
+    int i,j,k,count=0,flag=0,pn,fn;
+    int pages[20],frames[20];
+    
+    printf("Enter No of pages: ");
+    scanf("%d",&pn);
+    
+    for(i=0;i<pn;i++){
+        printf("Enter Page no %d: ",i+1);
+        scanf("%d",&pages[i]);
+    }
+    
+    printf("Enter No of Frames: ");
+    scanf("%d",&fn);
+    
+    for(i=0;i<fn;i++){
+        frames[i] = 0;
+    }
+    
+    j = 0;
+    
+    printf("\nPages: ");
+    for(i=0;i<pn;i++){
+        printf("%d ",pages[i]);
+    }
+    
+    printf("\nFrames: \n");
+    for(i=0;i<pn;i++){
+        flag=0;
+        for(k=0;k<fn;k++)
+            if(pages[i]==frames[k])
+                flag = 1;
+        if(flag==0){
+            count ++;
+            frames[j] = pages[i];
+            j = (j+1)%fn;
+            for(k=0;k<fn;k++){
+                printf("%d ",frames[k]);
             }
             printf("\n");
         }
-        printf("Page Fault is %d",count);
-        //getch();
+    }
+    
+    printf("\nNo of Page Faults = %d",count);
 }
